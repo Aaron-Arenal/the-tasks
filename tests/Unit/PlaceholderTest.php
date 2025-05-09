bac2618bbff595e153d334c36271afaf681b202a
Aarón Arenal <90368135+Aaron-Arenal@users.noreply.github.com>
2025-05-08 20:10:33 -0600

    Create PlaceholderTest.php
    Creado un test como placeholder, para evitar que un job falle de GitHub :(

---
+++ tests/Unit/PlaceholderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    public function testPlaceholder(): void
+    {
+        $this->assertTrue(true);
+    }
+}
